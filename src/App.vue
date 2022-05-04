5f8f277cfab6ac011db3230a7b9431bc391ab49e
abdelsalam <abdelsalam.abakar@yahoo.fr>
2022-05-04 09:08:40 +0200

    the first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+   <h1 class="text-center">{{ tittle }}</h1>
+   <Calculator />
+</template>
+
+<script>
+import Calculator from "./components/Calculator.vue";
+
+export default {
+   name: "App",
+   components: {
+      Calculator,
+   },
+   data() {
+      return {
+         tittle: "Test Calculator VueJs",
+      };
+   },
+};
+</script>
+
+<style>
+body {
+   background-color: #19c88d;
+}
+</style>
